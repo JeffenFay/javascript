76f247e07247c4b2ba09f172e197c37d446add7b
JeffenFay <jeffen.fay@gmail.com>
2018-10-03 16:42:17 +0200

    Premier lot d'exercices 1&2

---
+++ javascript/index1js.js
@@ -0,0 +1,3 @@
+var a;
+a="Improvise. Adapt. Overcome";
+alert(a);
---
+++ javascript/index2js.js
@@ -0,0 +1,5 @@
+var lastname, firstname, city;
+lastname="Doe";
+firstname="John";
+city="New-York";
+alert("Nom : "+lastname+"\nPrénom : "+firstname+"\nVille : "+city);
